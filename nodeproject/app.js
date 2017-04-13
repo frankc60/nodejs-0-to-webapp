b9c6e9f0429639e83790b84a055873bb4c3214ea
Frank <frankc60@gmail.com>
2017-04-13 11:27:45 +0000

    tidy up dir setting, get to section 3, 27 Config resources

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,26 @@
-var x = "hello world";
-console.log(x);
+var express = require("express");
+var app = express();
+
+var port = process.env.PORT; //needed by cloud9
+
+app.use(express.static("public"));
+//eg. https://mean-stack-frankc60.c9users.io/css/agency.css
+
+app.use(express.static("src/views"));
+//eg. https://mean-stack-frankc60.c9users.io/index.html
+
+
+//routing example
+app.get("/", function(req, res) {
+    res.send("hi Frank!");
+});
+
+app.get("/routing", function(req, res) {
+    res.send("hi routing!");
+});
+
+
+
+app.listen(port, function(err) {
+   console.log("the server is running on port " + port); 
+});
