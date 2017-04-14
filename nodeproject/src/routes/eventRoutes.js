f1fb06ba7412623cb2e1e5ea03bc30ccace84835
Frank <frankc60@gmail.com>
2017-04-14 12:48:54 +0000

    section 3: lecture 51, query DB onto events page

--- nodeproject/src/routes/eventRoutes.js
+++ nodeproject/src/routes/eventRoutes.js
@@ -1,42 +1,33 @@
+var mongodb = require("mongodb").MongoClient;
 var express = require("express");
-
 var eventRouter = express.Router();
 
-var eventsData = [
-    {
-        date: "1/12/16",
-        time: "12:05",
-        name: "First event",
-        description: "blah blah blah blah blah blah blah blah blah blah blah blah blah blah. End."
-    },
-    {
-        date: "2/12/16",
-        time: "11:16",
-        name: "2nd event",
-        description: "blah blah .dd"
-    },
-    {
-        date: "3/12/16",
-        time: "15:30",
-        name: "3rd event",
-        description: "blah blah blah blah.xx "
-    }
-];
+
+
 
 eventRouter.route("/") //https://mean-stack-frankc60.c9users.io/Events/
     .get(function(req,res) {
-        res.render("events", {  
-            list: ["aa val", "bb value", "3rd value"],
-            nav: [
-                {Link: "Services", Text: "Services"}, 
-                {Link: "Portfolio", Text: "Portfolio"}, 
-                {Link: "About", Text: "About"}, 
-                {Link: "Team", Text: "Team"}, 
-                {Link: "Contact", Text: "Contact"}, 
-                {Link: "Events", Text: "Events"}
-            ],
-            events: eventsData
+        var url = "mongodb://localhost:27017/eventsApp";
+        mongodb.connect(url, function(err, db) {
+            var collection = db.collection("events");
+            collection.find({}).toArray(function(err, results) {
+                res.render("events", {  
+                    list: ["aa val", "bb value", "3rd value"],
+                    nav: [
+                        {Link: "Services", Text: "Services"}, 
+                        {Link: "Portfolio", Text: "Portfolio"}, 
+                        {Link: "About", Text: "About"}, 
+                        {Link: "Team", Text: "Team"}, 
+                        {Link: "Contact", Text: "Contact"}, 
+                        {Link: "Events", Text: "Events"}
+                    ],
+                    events: results
+                });  
+            })
+           
         });
+        
+      
     });
     
   
